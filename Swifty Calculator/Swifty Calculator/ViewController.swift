ee4072100ce7ab10d76500f9295645f0f553a0f6
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-07-06 21:06:13 -0500

    Initialized Project

---
+++ Swifty Calculator/Swifty Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swifty Calculator
+//
+//  Created by Akshay Ramesh on 7/6/20.
+//  Copyright © 2020 Akshay Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
